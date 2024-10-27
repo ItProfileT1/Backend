5bfbd0b72c39b463e47729b77164f27e41f8e0be
brodep <brodep08@gmail.com>
2024-10-27 05:24:12 +0300

    added additional info to migrations

--- src/main/resources/db/migration/V2__insert_values.sql
+++ src/main/resources/db/migration/V2__insert_values.sql
@@ -4,4 +4,11 @@ insert into roles (name)
 values ('ROLE_ADMIN'), ('ROLE_USER');
 
 insert into users(username, password, role_id)
-values ('admin', '$2a$10$olLZfxwgaD1N9e4GIDoBIe6tQgu80Sphyg.kqSzum3VISvMOhwlxS', '1')
+values ('admin', '$2a$10$olLZfxwgaD1N9e4GIDoBIe6tQgu80Sphyg.kqSzum3VISvMOhwlxS', '1');
+
+insert into skill_levels(name, numeric_value)
+values ('Негативное проявление компетенции', -1),
+       ('Компетенция не развита', 0),
+       ('Базовый уровень владения компетенцией', 1),
+       ('Продвинутый уровень владения компетенцией', 2),
+       ('Экспертный уровень владения компетенцией', 3)
