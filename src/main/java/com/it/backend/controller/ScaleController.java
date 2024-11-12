c7cd7c36f90fdb23494b974293778e365c78c9a3
brodep <brodep08@gmail.com>
2024-11-12 09:45:38 +0300

    Created ScaleController

---
+++ src/main/java/com/it/backend/controller/ScaleController.java
@@ -0,0 +1,15 @@
+package com.it.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/scale")
+public class ScaleController {
+
+//    public ScaleResponse findAllScales(){
+//
+//    }
+}
